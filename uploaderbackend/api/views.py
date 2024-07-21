0e3fcb092a81dd7c26bc3a1e69c35de93bf3c510
RiturajAgrahari <agr.rituraj@gmail.com>
2024-07-21 11:35:53 +0530

    completed whole workflow for rar files

--- uploaderbackend/api/views.py
+++ uploaderbackend/api/views.py
@@ -70,20 +70,21 @@ def upload_server(request, *args, **kwargs):
                         for files in list_files:
                             if files.endswith(".x86_64"):
                                 subprocess.run(f'chmod +x ./server/{correct_format}/{files}', shell=True)
-                                info_logger.info(correct_format)
 
                                 subprocess.run("rm -r nohup.out", shell=True)
 
                                 global online_server_pid
 
                                 if online_server_pid:
-                                    subprocess.run(f"kill {online_server_pid}")
+                                    info_logger.info(f"old process PID :{online_server_pid}")
+                                    subprocess.run(f"kill {online_server_pid}", shell=True)
+                                    info_logger.info(f"old process PID :{online_server_pid} killed!")
 
                                 with open('nohup.out', 'w') as f:
                                     cmd = f'./server/{correct_format}/{files}'
                                     process = subprocess.Popen(cmd, shell=True, stdout=f, start_new_session=True)
                                     online_server_pid = process.pid + 1
-                                    info_logger.info(process.pid)
+                                    info_logger.info(f"server PID: {online_server_pid} is online now!")
 
                                 return Response({"message": "server is started!"}, status=200)
 

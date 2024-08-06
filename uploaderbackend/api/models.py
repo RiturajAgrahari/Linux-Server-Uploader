566171cb43ebe30fb26504f88af822ebaf801ea1
RiturajAgrahari <agr.rituraj@gmail.com>
2024-08-06 21:16:29 +0530

    improved some code

--- uploaderbackend/api/models.py
+++ uploaderbackend/api/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class ServerHistory(models.Model):
-    class Meta:
-        verbose_name_plural = "Server History"
-
     id = models.BigIntegerField(verbose_name="id", primary_key=True)
     pid = models.IntegerField(verbose_name="PID", null=False)
     server_name = models.CharField(verbose_name="server name", null=False, max_length=50)
     status = models.CharField(verbose_name="status", null=False, max_length=10)
     upload_time = models.DateTimeField(verbose_name="uploaded on", auto_now_add=True)
     stop_time = models.DateTimeField(verbose_name="stopped on", auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Server History"
